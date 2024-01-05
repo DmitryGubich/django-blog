e1d2921cb54e2044585566f2b7bae596a2bb90c4
Dmitry Gubich <gubich97@gmail.com>
2024-01-05 23:00:08 +0100

    add table support

--- src/templatetags.py
+++ src/templatetags.py
@@ -2,6 +2,7 @@ import markdown
 from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
+from markdown.extensions.tables import TableExtension
 
 register = template.Library()
 
@@ -9,5 +10,11 @@ register = template.Library()
 @register.filter()
 @stringfilter
 def render_markdown(value):
-    md = markdown.Markdown(extensions=["fenced_code", "codehilite"])
+    md = markdown.Markdown(
+        extensions=[
+            "fenced_code",
+            "codehilite",
+            TableExtension(use_align_attribute=True),
+        ]
+    )
     return mark_safe(md.convert(value))
